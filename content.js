c5e30762551c64ec6b981881d49ef7c8afe1145e
Bilal <bilalkamal@users.noreply.github.com>
2024-05-18 17:47:59 +0300

    fixed minor bug

--- content.js
+++ content.js
@@ -28,15 +28,41 @@ function fixAudioTrack(settingsButton) {
 
     setTimeout(() => {
       const audioOptions = document.querySelectorAll(".ytp-menuitem");
+      let originalFound = false;
       for (const option of audioOptions) {
         const label = option.querySelector(".ytp-menuitem-label");
         if (label && label.textContent.includes("original")) {
           option.click();
+          originalFound = true;
           break;
         }
       }
-    }, 50);
-  }, 50);
+
+      if (!originalFound) {
+        document.dispatchEvent(
+          new MouseEvent("click", {
+            bubbles: true,
+            cancelable: true,
+            view: window,
+            clientX: 0,
+            clientY: 0,
+          })
+        );
+      } else {
+        document.dispatchEvent(
+          new KeyboardEvent("keydown", {
+            key: "Escape",
+            code: "Escape",
+            keyCode: 27,
+            which: 27,
+            bubbles: true,
+            cancelable: true,
+            view: window,
+          })
+        );
+      }
+    }, 30);
+  }, 30);
 }
 
 waitForElement(".ytp-settings-button", fixAudioTrack);
